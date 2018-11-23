07ebcbdcbbbdf19a30335673ab47fc8a8faaac64
seattlegirl <likaiyue@i.shu.edu.cn>
2018-11-23 18:32:57 +0800

    Signed-off-by: seattlegirl <likaiyue@i.shu.edu.cn>

---
+++ find-smallest-letter-greater-than-target.py
@@ -0,0 +1,66 @@
+#coding=utf-8
+
+"""
+给定一个只包含小写字母的有序数组letters 和一个目标字母 target，寻找有序数组里面比目标字母大的最小字母。
+
+数组里字母的顺序是循环的。举个例子，如果目标字母target = 'z' 并且有序数组为 letters = ['a', 'b']，则答案返回 'a'。
+
+示例:
+
+输入:
+letters = ["c", "f", "j"]
+target = "a"
+输出: "c"
+
+输入:
+letters = ["c", "f", "j"]
+target = "c"
+输出: "f"
+
+输入:
+letters = ["c", "f", "j"]
+target = "d"
+输出: "f"
+
+输入:
+letters = ["c", "f", "j"]
+target = "g"
+输出: "j"
+
+输入:
+letters = ["c", "f", "j"]
+target = "j"
+输出: "c"
+
+输入:
+letters = ["c", "f", "j"]
+target = "k"
+输出: "c"
+注:
+
+letters长度范围在[2, 10000]区间内。
+letters 仅由小写字母组成，最少包含两个不同的字母。
+目标字母target 是一个小写字母。
+
+
+题目描述：给定一个有序的字符数组 letters 和一个字符 target，要求找出 letters 中大于 target 的最小字符，如
+果找不到就返回第 1 个字符
+"""
+
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for letter in letters:
+            if letter>target:
+                return letter
+        return letters[0]
+
+if __name__ == "__main__":
+    letters = ["c", "f", "j"]
+    target = "a"
+    print Solution().nextGreatestLetter(letters,target)
+
